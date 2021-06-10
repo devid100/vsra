0a1ecb86dbce2be931fc39bec0edfba68301d812
devid100 <82916614+devid100@users.noreply.github.com>
2021-06-10 03:15:57 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -4311,6 +4311,18 @@ end
 send(msg.chat_id_, msg.id_, t)
 end
 
+if text == "نيك الروم" and SudoBot(msg) then 
+tdcli_function({ID = "GetChannelMembers",channel_id_ = msg.chat_id_:gsub( -100 ,  ), offset_ = 0,limit_ = 1000 
+},function(ta,DRAGON) 
+x = 0 
+local list = DRAGON.members_ 
+for k, v in pairs(list) do 
+if tonumber(v.user_id_) ~= tonumber(bot_id) then 
+chat_kick(msg.chat_id_,v.user_id_) 
+end 
+end 
+end,nil) 
+end
 
 if text == "@all" or text == "تاك للكل" or text == "all" and CoSu(msg) then
 if not database:get(bot_id..'Cick:all'..msg.chat_id_) then
